73721791ce5571d3d182073ca22d2a82f871d05d
ForestStudios <122174925+ForestStudios@users.noreply.github.com>
2023-01-07 10:27:30 -0800

    Add files via upload

---
+++ js/mine.js
@@ -0,0 +1,51 @@
+// var Myvar, Myvar_2, Myvar_3;
+// let Myvar, Myvar_2, Myvar_3;
+// const Myvar = 15;
+
+
+// let number = Number  15, 58, 10.10, 586, 250; 
+// let string = String  'giorgi dvalishvili 250';
+// let bool = Boolean  true false ;
+// let undefie = undefined  cvladi romelsac mnishvneloba ar aqvs minichebuli
+// let nul = null  ganusazRvreli mnishvneloba
+// let nan = NaN not a number
+// let objec = Object obieqti
+
+
+// alert(Myvar);
+
+
+
+// let a = "10";
+// let b = 100;
+
+let a = 'Name = David';
+let b = 'Surname = Forrest';
+let c = "Age = 14"
+let d = 'Married = False'
+
+// let result = name + " " + surname + " " + "hi";
+
+// let result = ( parseInt(a) + parseInt(b) ) * 15;
+
+// let result = a !== b;
+
+// let result = a >= b;
+
+// && - და
+
+// || - ან
+
+// let result = a <= b && a > 50 && b == 100;
+
+// let result = a >= b || a > 50 || b == 100;
+
+// let result = a + " " + b + " " + c + " " + d
+let resulta = a
+let resultb = b
+let resultc = c
+let resultd = d
+console.log(resulta);
+console.log(resultb);
+console.log(resultc);
+console.log(resultd);
